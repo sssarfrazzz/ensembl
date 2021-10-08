75a2efd20fe4e0dbaebf97ee760427419224ccbe
sssarfrazzz <sarfrazshaikh14@gmail.com>
2021-10-08 12:38:31 +0530

    Assembly converter of chromosome 10 from GRCh38 to GRCh37 added

---
+++ GRCH38to37_chromosome10.pl
@@ -0,0 +1,23 @@
+use strict;
+use warnings;
+use Bio::EnsEMBL::Registry;
+
+
+my $registry = 'Bio::EnsEMBL::Registry';
+
+#port 3337 is GRCh27
+#port 3306 is for ensemble 48 onwords
+$registry->load_registry_from_db(
+    -host => 'ensembldb.ensembl.org', # alternatively '   .ensembl.org'
+    -user => 'anonymous',
+    -port => 3306
+);
+
+my $slice_adaptor = $registry->get_adaptor('Human','Core','Slice');
+
+my $slice=$slice_adaptor->fetch_by_region('chromosome','10',25000,30000);
+printf( "#input slice %s\n", $slice->name() );
+print "\n new Projections \n";
+foreach my $segment ( @{ $slice->project('chromosome','GRCh37') } ) {
+print   $segment->to_Slice()->name(),"\n" ;
+}
